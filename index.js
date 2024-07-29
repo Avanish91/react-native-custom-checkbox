1f98186405ecfc65359e5fafead60898ef11b127
Avanish Tiwari <avanishtiwari1991@gmail.com>
2024-07-29 23:39:56 +0530

    icon customize option add

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ const Checkbox =(props)=> {
             onChange         :  props.hasOwnProperty('onChange')?props.onChange:null,
             size             :  props.hasOwnProperty('size')?props.size:20,
 			borderColor      :  props.hasOwnProperty('borderColor')?props.borderColor:'#000',
-            iconType         :  props.hasOwnProperty('iconType')?props.iconType:'check'
+            iconType         :  props.hasOwnProperty('iconType')?props.iconType:'check',
+            iconSize         :  props.hasOwnProperty('iconSize')?props.iconSize:props.hasOwnProperty('size')?props.size - 5:15,
+            iconColor        :  props.hasOwnProperty('iconColor')?props.iconColor:'#000'
         });
         const toggleCheck =()=> {
             var checked = !state.checked;
             setState({ ...state,checked: checked });
         }
-        const [BACKGROUND_COLOR,BORDER_RADIUS,BORDER_WIDTH,COLOR,MARGIN,SIZE,BORDER_COLOR,ICON_TYPE]=[state.backgroundColor,state.borderRadius,state.borderWidth,state.color,state.margin,state.size,state.borderColor,state.iconType] 
-
+        const [BACKGROUND_COLOR,BORDER_RADIUS,BORDER_WIDTH,COLOR,MARGIN,SIZE,BORDER_COLOR,ICON_TYPE,ICON_SIZE,ICON_COLOR]=[state.backgroundColor,state.borderRadius,state.borderWidth,state.color,state.margin,state.size,state.borderColor,state.iconType,state.iconSize,state.iconColor] 
         return (
             <TouchableHighlight underlayColor='transparent'
                 onPress={() => { toggleCheck() }}
@@ -31,7 +32,7 @@ const Checkbox =(props)=> {
                 >
                 <View style={{flex: 1, alignItems: 'center'}}>
                     { state.checked &&
-                    <Icon name={ICON_TYPE} size={SIZE - 5 } color={COLOR}/> }
+                    <Icon name={ICON_TYPE} size={ICON_SIZE} color={ICON_COLOR}/> }
                 </View>
             </TouchableHighlight>
         );
